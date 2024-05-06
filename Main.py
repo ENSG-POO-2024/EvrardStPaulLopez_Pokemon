1d03c54193a0cbc7786996f9f53175e35d765f4d
aitorniz <aitorlopez@orange.fr>
2024-05-06 11:15:11 +0200

    Instanciation du conteneur Pokemons par conversion du .csv à l'aide de Pandas. Problème avec la surcharge de getitem qui renvoye tous les pokemons du conteneur au lieu de ne renvoyer que le pokemon correspondant à la clé. Problème avec la surcharge de remove, pop() ne semble pas marcher.

--- Main.py
+++ Main.py
@@ -8,19 +8,23 @@ import numpy as np
 #import de classes
 from Pokemon import *
 
-################################
-#####Instanciation##############
-################################
+######################################
+#####Instanciation des Pokemons ######
+######################################
 
-Salamèche = PokemonSauvage("Salamèche", (15, 452), "Feu", 200)
+Salamèche = PokemonSauvage("Salamèche", (15, 452), "Feu", "Eau", 200)
 print(Salamèche)
 
 G1 = Pokemons("Génération 1")
-print(G1)
-Bulbizard = G1['Bulbasaur']
+#print(G1)
+
+Bulbizard = G1["Bulbasaur"]
+print("Afficher infos de Bulbizard")
 print(Bulbizard)
+
 print("Avant la suppresion de Bulbizard")
 print(len(G1))
-G1.remove("Bulbasaur")
+
+G1.remove(Bulbizard)
 print("Après la suppresion de Bulbizard")
 print(len(G1))
--- Pokemon.py
+++ Pokemon.py
@@ -17,21 +17,24 @@ class Pokemon:
         return
 
 class PokemonSauvage(Pokemon):
-    def __init__(self, nom, position, type, barreDeVie):
+    def __init__(self, nom, position, type1, type2, barreDeVie):
         self.position = position
-        self.type = type
+        self.type1 = type1
+        self.type2 = type2
         #il y aura 151 pokemons distincts donc
         #le nom peut-être la clé primaire
         #pour identifier l'instance Pokemon sauvage
         #au cours du jeu
         self.nom = nom
         self.barreDeVie = barreDeVie
+
     def attaquer(self, pokemonAttaque):
         """
         gèrer l'attaque faite par le pokemon
         """
         #faire appel à la classe attaque pour gèrer l'attaque
         return
+    
     def supprimer(self):
         #lorsque le pokemon est capturé
         #l'instance de Pokemon sauvage 
@@ -48,15 +51,17 @@ class PokemonSauvage(Pokemon):
     def __str__(self):
         txt = "Je m'appelle "
         txt += str(self.nom)
-        txt += ". Je suis de type " + str(self.type)
+        txt += ". Je suis de type1 " + str(self.type1)
+        txt += " et de type2 " + str(self.type2)
         txt += ". Je rôde en " + str(self.position)
         txt += ". J'ai " + str(self.barreDeVie) + " points de vie restants"
         return txt
     
 class PokemonDresseur(Pokemon):
-    def __init__(self, nom, type, barreDeVie):
+    def __init__(self, nom, type1, type2, barreDeVie):
         self.nom = nom
-        self.type = TypeError
+        self.type1 = type1
+        self.type2 = type2
         self.barreDeVie = barreDeVie
 
     def attaquer(self, pokemonAttaque):
@@ -73,24 +78,32 @@ class PokemonDresseur(Pokemon):
         txt += ". Je suis de type " + str(self.type)
         #un pokemon domestique n'a pas de position
         #il est dans le deck du dresseur
-        txt += ". J'ai " + str(self.barreDeVie) + " points de vie restants"
+        txt += ". J'ai " + str(self.barreDeVie) + " points de vie restant"
         return txt
 
 class Pokemons(PokemonSauvage):
     def __init__(self, nom):
-        self.nom = "Generation 1"
-        self.Pokemons = []
+        self.nom = nom
         tableau_pokemon = pd.read_csv("./EvrardStPaulLopez_Pokemon/data/pokemon_first_gen.csv")
         #on crée l'objet Pokemon dans une colonne du tableau
-        tableau_pokemon['Pokemon'] = PokemonSauvage(tableau_pokemon['Name'], (rd.random, rd.random), tableau_pokemon['Type 1'], tableau_pokemon['HP'])
+        tableau_pokemon['ObjetPokemon'] = PokemonSauvage(tableau_pokemon['Name'], (rd.random, rd.random), tableau_pokemon['Type 1'], tableau_pokemon['Type 2'],tableau_pokemon['HP'])
         #à ce stade, la position est aléatoire
-        self.Pokemons = tableau_pokemon['Pokemon'].tolist()
-        
+
+        self.Pokemons = {}
+        #on transforme les Series "Nom", nos clés en liste
+        #et les Series d'Objet PokemonSauvage en liste
+        #elles deviennent subscriptables et ajoutables
+        nomPokemon = tableau_pokemon['Name'].tolist()
+        objetPokemon = tableau_pokemon['ObjetPokemon'].tolist()
+        for i in range(len(tableau_pokemon['#'].tolist())): #on itère sur l'indice du Pokemon
+            self.Pokemons[nomPokemon[i]] = objetPokemon[i]
+
     def __len__(self):
         """
         obtenir le nombre de pokemons sauvages présents dans l'objet
         """
         return len(self.Pokemons)
+    
     def __str__(self):
         """
         afficher les informations utiles du groupe de Pokemons
@@ -98,17 +111,31 @@ class Pokemons(PokemonSauvage):
         txt = "Nous sommes les Pokemons de la "
         txt += self.nom
         return txt
+    
     def remove(self, nomPokemon):
         """
         Supprimer un Pokemon sauvage lorsqu'il est capturé
         """
         if isinstance(nomPokemon, Pokemon):
-            del self.Pokemons.pop(nomPokemon)
+            self.Pokemons.pop(nomPokemon)
+        else:
+            print(str(nomPokemon) + " n'est pas un pokemon.")
         return
     
-    def __getitem__(self, nomPokemon):
+    def __getitem__(self, clePokemon):
         """
         récupèrer un Pokemon à partir de son nom (puisque chaque instance est unique)
+        renvoyer diff selon type de l'entrée entre crochet
+        si coord renvoyer ça, si str renvoyer ça, si type renvoyer types, 
         """
-        if isinstance(nomPokemon, Pokemon):
-            return self.Pokemons[nomPokemon]
+        if isinstance(clePokemon, str):
+            Pokemon = self.Pokemons[clePokemon]
+            return Pokemon
+        else:
+            print(str(clePokemon) +" n'est pas un pokemon.")
+
+    def __add__(self, Pokemon):
+        """
+        fonction permettant d'ajouter un Pokemon à l'ensemble des pokemons
+        """
+        self.Pokemons += Pokemon
